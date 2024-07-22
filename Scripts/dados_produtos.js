bc309aaafea587344ede045d75ed22868e45c6b4
DiegoOliveira01 <diegofontes.002@gmail.com>
2024-07-22 20:00:20 -0300

    Improved General Navigation, Photos And Data
    Improved Javascript on main page to be easier to manage prices and names of the products, Added More Products photos and improved the sizes of the old Photos

--- Scripts/dados_produtos.js
+++ Scripts/dados_produtos.js
@@ -1,15 +1,29 @@
 const nome = {
-    Cinta: "Cinta Redutora Massage Belt Cinta",
+    Cinta: "Cinta Redutora Massage Belt",
+    Corporal_Amarelo: "Corporal De Massagem Amarelo",
+    Corporal_Prata: "Corporal De Massagem Prata",
+    Plataforma: "Plataforma Vibratória Body Simmer Physical",
     Almofada: "Almofada Vibratória Térmica",
-    Massageador: "Massageador Hand Black Physical"
+    Massageador_Black: "Massageador Hand Black Physical",
+    Colchao_Max: "Physic MAX rabatan associado às pastilhas de infravermelho longo e pastilhas magnéticas",
+    Esteira_Cromo: "Esteira Cromo Plus",
+    Esteira_Rest: "Esteira Physical Rest",
+    Foot_Massage: "Foot Massage II",
+
 };
 
 const precos = {
     Cinta: "R$ 620,00 ou 10X de R$62,00",
-    Colete: "R$ 1.199,00",
-    Massageador: "R$ 620,00",
-    produto4: "R$ 999,00",
-    produto5: "R$ 1.299,00"
+    Corporal_Amarelo: "Preço Sob Consulta",
+    Corporal_Prata: "Preço Sob Consulta",
+    Plataforma: "R$ 2.199,00", 
+    Almofada: "R$1.199,99",
+    Massageador_Black: "R$ 620,00",
+    Colchao_Max: "Preço Sob Consulta",
+    Esteira_Cromo: "R$ 1.970,00",
+    Esteira_Rest: "R$ 2.890,00",
+    Foot_Massage: "R$ 690,00",
+
 };
 
 const links = {
